7661b72f238deaaa5792b5f9a4aa1720a2d91ef7
RandomizedRaptor <i.mseriousstuff@gmail.com>
2024-03-12 11:20:56 -0400

    debut swiper lp

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper-screenshots', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    centeredSlides: true,
+
+    breakpoints: {
+        1200: {
+        slidesPerView: 2,
+        spaceBetween: 25,
+      },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
