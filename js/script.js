3f0073bd9ad0cdfe83a1eb9c3eee5a88a90fc7d0
RandomizedRaptor <i.mseriousstuff@gmail.com>
2024-03-18 10:20:06 -0400

    code formatte

--- js/script.js
+++ js/script.js
@@ -1,31 +1,26 @@
+const mySwiper = new Swiper(".swiper-screenshots", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 1,
+  watchSlidesProgress: true,
 
-
-
-const mySwiper = new Swiper('.swiper-screenshots', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    centeredSlides: true,
-    slidesPerView: 1,
-    watchSlidesProgress: true,
-
-    breakpoints: {
-        1200: {
-        slidesPerView: 3,
-        spaceBetween: 25,
-      }},
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 25,
     },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
-
-
-
-const swiper_filters = new Swiper('.swiper-filters', {
+const swiper_filters = new Swiper(".swiper-filters", {
   direction: "horizontal",
   slidesPerView: "auto",
   freeMode: true,
@@ -34,13 +29,9 @@ const swiper_filters = new Swiper('.swiper-filters', {
   initialSlide: 2,
 });
 
-
-const swiper_featured = new Swiper('.swiper-featured-artworks', {
+const swiper_featured = new Swiper(".swiper-featured-artworks", {
   direction: "horizontal",
   slidesPerView: 2.5,
   freeMode: true,
   spaceBetween: 80,
 });
-
-
-
